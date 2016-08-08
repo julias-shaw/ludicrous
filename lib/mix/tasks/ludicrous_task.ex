ec51ba814d0c4134e85504903f4dc5dce5ad8494
Julias Shaw <julias@juliasshaw.com>
2016-08-08 16:01:35 -0700

    Added intital ludicrous task

---
+++ lib/mix/tasks/ludicrous_task.ex
@@ -0,0 +1,15 @@
+defmodule Mix.Tasks.Ludicrous do
+  use Mix.Task
+
+  @shortdoc "Ludicrous mode commits and pushes automatically."
+  def run(_) do
+    IO.puts "Ludicrous Mode Engaged!"
+    System.cmd("git", ["add", "-A"])
+    System.cmd("git", ["commit", "-m", "Ludicrous mode commit!"])
+    {_, result_code } = System.cmd("ping", ["-c", "1", "-q", "github.com"])
+    case result_code do
+      0 -> System.cmd("git", ["push", "origin", "master"])
+      _ -> IO.puts("Network not available - Not pushing :(")
+    end
+  end
+end
